f1e497f0181323d3b2158a3dd0e16ce66df781d7
divya1388 <konasani.divya@gmail.com>
2019-04-11 16:20:03 -0400

    attached a SZ allowing ssh to the instance

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -21,14 +21,14 @@ output "public_ip" {
   value = ["${aws_instance.web.public_ip}"]
 } */
 
-
 # Provision Ec2 by generating SSH Key
 resource "aws_instance" "web" {
-  ami           = "${lookup(var.instance_ami, var.aws_region)}"
-  instance_type = "${var.instance_type}"
-  key_name      = "${aws_key_pair.ssh_key.key_name}"
+  ami             = "${lookup(var.instance_ami, var.aws_region)}"
+  instance_type   = "${var.instance_type}"
+  key_name        = "${aws_key_pair.ssh_key.key_name}"
+  security_groups = ["${aws_security_group.allow_ssh.name}"]
 }
 
 output "public_ip" {
   value = ["${aws_instance.web.public_ip}"]
-  }
+}
